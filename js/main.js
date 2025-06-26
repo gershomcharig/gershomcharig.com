d06c239a5000e0fc6ab20250c2419abe3057ad54
gershomcharig <gershom.charig@gmail.com>
2025-06-26 22:08:26 +0100

    Implemented imagesLoaded
    Unloaded images can throw off Masonry layouts and cause item elements to overlap. imagesLoaded resolves this issue.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ console.log('Simple website loaded.');
 document.addEventListener('DOMContentLoaded', function() {
   var grid = document.querySelector('.grid');
   if (grid) {
-    new Masonry(grid, {
-      itemSelector: '.grid-item',
-      columnWidth: 300,
-      gutter: 16,
-      fitWidth: true
+    imagesLoaded(grid, function() {
+      new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-item',
+        gutter: 16,
+        percentPosition: true
+      });
     });
   }
 }); 
