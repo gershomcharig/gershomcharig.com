c676d9a3d7f087a5d5e71638557c0c2ad6f686e4
gershomcharig <gershom.charig@gmail.com>
2025-06-27 18:22:37 +0100

    Reverted a bug fix that didn't fix

--- js/main.js
+++ js/main.js
@@ -10,20 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const gutter = parseInt(gutterSize);
 
     imagesLoaded(grid, function() {
-      var msnry = new Masonry(grid, {
+      new Masonry(grid, {
         itemSelector: '.grid-item',
         columnWidth: '.grid-sizer',
         gutter: gutter,
         percentPosition: true
       });
-
-      // Use ResizeObserver for the grid container
-      if ('ResizeObserver' in window) {
-        const ro = new ResizeObserver(() => {
-          msnry.layout();
-        });
-        ro.observe(grid);
-      }
     });
   }
 }); 
